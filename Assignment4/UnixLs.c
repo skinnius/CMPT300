825b9866b84a74b903543f8e3d4c9aa710dc0faa
mla387 <mla387@sfu.ca>
2024-04-12 05:51:16 -0700

    properly parsing user commands:

--- Assignment4/UnixLs.c
+++ Assignment4/UnixLs.c
@@ -3,11 +3,20 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 #include <grp.h>
 #include <pwd.h>
 #include <dirent.h>
 #include <sys/stat.h>
+
+// valid flag checker
+bool isValidFlagChar(char c) {
+    if (c == 'l' || c == 'i') {
+        return true;
+    }
+    return false;
+}
 
 // for basic ls command
 void printBasicDir(char* entryDir[], int len) {
@@ -45,21 +54,71 @@ void printBasicDir(char* entryDir[], int len) {
 
 // -i, --inode = print the index number of each file
 
+void printList(char* list[]) {
+    int i = 0;
+    while (list[i] != 0) {
+        printf("%s\n", list[i]);
+        i++;
+    }
+}
+
 
 int main(int argc, char *argv[]) {
-    bool dir;
+    bool isFlag = true;
     int numFlags = argc - 1;
-    
+    char* dirList[numFlags];
+    char* flagList[numFlags];   
+    int dirListIndex = 0;
+    int flagListIndex = 0;
+
+
+    memset(dirList, 0, sizeof(dirList));
+    memset(flagList, 0, sizeof(flagList));
+
+
     if (numFlags == 0) {                    // basic ls
         char* d[1] = {"."};
         printBasicDir(d, 1);
         return 0;    
     }
-
-    char* argList[numFlags];        
+     
     for (int i = 1; i < argc; i++) {        // get all arguments from command line
-        argList[i-1] = argv[i];
+        if (argv[i] == NULL) {
+            continue;
+        }
+
+        int j = 0;
+        char c = argv[i][j];                // parse through string to determine if it is a valid flag. 
+        if (c != '-') {
+            isFlag = false;
+        }
+
+        c = argv[i][++j];
+
+        while (c != '\0') {
+            if (isFlag && !isValidFlagChar(c)) {
+                isFlag = false;
+            }
+            c = argv[i][++j];
+        }
+
+        if (isFlag) {
+            flagList[flagListIndex] = argv[i]; 
+            flagListIndex++;
+        }
+        else {
+            dirList[dirListIndex] = argv[i];
+            dirListIndex++;
+        }
     }
+    printf("------- flags: -------------\n");
+    printList(flagList);
+
+    printf("--------dir------------\n");
+    printList(dirList);
+
+
+
 
 
     return 0;
