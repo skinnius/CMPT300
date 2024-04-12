a6f75bf29e19b93cc3d4c664c9c4d7b6ba88e50d
mla387 <mla387@sfu.ca>
2024-04-12 05:23:31 -0700

    .

--- Assignment4/UnixLs.c
+++ Assignment4/UnixLs.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/types.h>
@@ -7,43 +9,58 @@
 #include <dirent.h>
 #include <sys/stat.h>
 
-
-void printList(int n, char* list[]) {
-    for (int i = 0; i < n - 1; i++) {
-        printf("%s\n", list[i]);
-    }
-}
-
-void printDir() {
+// for basic ls command
+void printBasicDir(char* entryDir[], int len) {
     struct stat buf;
     DIR* dir;
     struct dirent *dp;
 
-    dir = opendir(".");    
-    while ((dp = readdir(dir)) != NULL) {
-        int status = stat(dp->d_name, &buf);
+    for (int i = 0; i < len; i++) {
+        dir = opendir(entryDir[i]);
 
-        if (status == -1) {
+        if (dir == NULL) {
             printf("an error occured\n");
             continue;
         }
 
-        printf("%s\n", dp->d_name);
+        while ((dp = readdir(dir)) != NULL) {
+            if (dp->d_name[0] == '.') { // ensures that hidden files are not displayed
+                continue;
+            }
+            int status = stat(dp->d_name, &buf);
+            if (status == -1) {
+                printf("an error occured when retrieving the info of this file\n");
+                continue;
+            }
+            printf("%s\n", dp->d_name);
+        }
+        printf("\n");
     }
 
-
+    if (dir != NULL) {
+        closedir(dir);
+    }
 }
 
 
+// -i, --inode = print the index number of each file
+
 
 int main(int argc, char *argv[]) {
+    bool dir;
+    int numFlags = argc - 1;
+    
+    if (numFlags == 0) {                    // basic ls
+        char* d[1] = {"."};
+        printBasicDir(d, 1);
+        return 0;    
+    }
 
-    char* argList[argc - 1];        
+    char* argList[numFlags];        
     for (int i = 1; i < argc; i++) {        // get all arguments from command line
         argList[i-1] = argv[i];
     }
 
-    printDir();
 
     return 0;
 }
